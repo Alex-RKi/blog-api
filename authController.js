460b72bf7c257b75513a8695d0f2c1cb89d401fc
Alex RKi <aleksandr.ksj@mail.ru>
2021-04-10 00:05:33 +0300

    Authorization logic complete

--- authController.js
+++ authController.js
@@ -1,10 +1,23 @@
 const Role = require("./models/Role");
 const User = require("./models/User");
 const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const { secret } = require("./config");
+
+const generateAccessToken = (id, roles) => {
+  const payload = { id, roles };
+  return jwt.sign(payload, secret, { expiresIn: "24h" });
+};
 
 class authController {
+  //reg
   async registration(req, res) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: "Validation error", errors });
+      }
       const { username, password } = req.body;
       const candidate = await User.findOne({ username });
       if (candidate) {
@@ -24,15 +37,30 @@ class authController {
       res.status(400).json({ meassage: "Registration error" });
     }
   }
+  //login
   async login(req, res) {
     try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res.status(200).json({ message: `User ${username} not found` });
+      }
+      const validPassword = bcrypt.compareSync(password, user.password);
+      if (!validPassword) {
+        return res.status(400).json({ message: "Wrong username or password" });
+      }
+      const token = generateAccessToken(user._id, user.roles);
+      return res.json({ token });
     } catch (e) {
       console.log(e);
       res.status(400).json({ meassage: "Login error" });
     }
   }
+  //users list
   async getUsers(req, res) {
     try {
+      const users = await User.find();
+      res.json(users);
       res.json("Works!");
     } catch (e) {
       console.log(e);
--- authRouter.js
+++ authRouter.js
@@ -1,9 +1,25 @@
 const Router = require("express");
 const controller = require("./authController");
+const { check } = require("express-validator");
+const authMiddleware = require("./middleware/authMiddleware");
+const roleMiddleware = require("./middleware/roleMiddleware");
 
 const router = new Router();
-router.post("/registration", controller.registration);
+router.post(
+  "/registration",
+  check("username", "Username can't be empty string").notEmpty(),
+  check("password", "Password must be between 4 and 10 characters").isLength({
+    min: 4,
+    max: 10,
+  }),
+  controller.registration
+);
 router.post("/login", controller.login);
-router.get("/users", controller.getUsers);
+router.get(
+  "/users",
+  roleMiddleware(["ADMIN"]),
+  authMiddleware,
+  controller.getUsers
+);
 
 module.exports = router;
